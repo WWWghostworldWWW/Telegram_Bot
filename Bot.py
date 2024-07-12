3ce3bfeaba11225154a7463b8d28fec4a67b0904
WWWghostworldWWW <kolchinsergey.1992@gmail.com>
2024-07-12 21:28:40 +0300

    bot

--- Bot.py
+++ Bot.py
@@ -1,11 +1,21 @@
 import telebot
 from currency_converter import CurrencyConverter
 
+TOKEN = '6893001021:AAFqblDDMntpGcii19ivfPmoVYShr5K4IDg'
 
-
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot(TOKEN)
 currency = CurrencyConverter()
 
+keys = {
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'JPY': 'JPY',
+}
 
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = ('Что бы перевести валюту введите команду в следующем формате:\n<имя валюты>  <в какую валюту переdрести> \
+    <количество переводимой валюты>')
+    bot.reply_to(message, text)
 
 bot.polling(non_stop=True)
